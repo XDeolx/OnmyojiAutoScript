88e967c6564967f92e95c2c660649c3f2620ff55
XzHyX <415511689@qq.com>
2026-09-09 20:47:15 +0800

    修复探索章节入口结界突破票数识别

--- tasks/Exploration/assets.py
+++ tasks/Exploration/assets.py
@@ -136,7 +136,7 @@ class ExplorationAssets:
 	# 探索右上角 突破卷的数量 
 	O_REALM_RAID_NUMBER = RuleOcr(roi=(739,11,78,37), area=(739,11,78,37), mode="DigitCounter", method="Default", keyword="", name="realm_raid_number")
 	# （点出困难28时候）探索右上角 突破卷的数量 
-	O_REALM_RAID_NUMBER1 = RuleOcr(roi=(936,10,82,36), area=(936,10,82,36), mode="DigitCounter", method="Default", keyword="", name="realm_raid_number1")
+	O_REALM_RAID_NUMBER1 = RuleOcr(roi=(924,10,100,36), area=(924,10,100,36), mode="DigitCounter", method="Default", keyword="", name="realm_raid_number1")
 
 
 	# Swipe Rule Assets
